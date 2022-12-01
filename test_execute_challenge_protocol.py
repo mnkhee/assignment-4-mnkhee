da692f49af793055fe1f04c7df007c6fcddfe8e5
Joshua Chuah <113268938+mnkhee@users.noreply.github.com>
2022-11-30 20:24:59 -0800

    fix typo

--- test_execute_challenge_protocol.py
+++ test_execute_challenge_protocol.py
@@ -19,7 +19,7 @@ class Test(TestCase):
         with patch('builtins.input', new=Mock(return_value='1')):
             execute_challenge_protocol(character)
 
-    def test_execute_challenge_protocol_level_run(self):
+    def test_execute_challenge_protocol_run(self):
         character = {'Level': 3, 'X': 2, 'Y': 2, 'XP': 0, 'Current_HP': 54, 'Max_HP': 54, 'Attack': 34, 'Defence': 20, 'Class': 'Basic', 'Move_Set': {'Run': 0, 'Punch': 34}}
         with patch('builtins.input', new=Mock(return_value='0')):
             execute_challenge_protocol(character)
